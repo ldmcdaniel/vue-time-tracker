9bf71858f3b6debc698bec5c0bed1cf622f3c151
Lucas McDaniel <ldmcdaniel@gmail.com>
2016-12-31 10:15:49 -0600

    Working vue app

--- src/main.js
+++ src/main.js
@@ -1,11 +1,23 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 import App from './App';
+import Hello from './components/Hello';
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App },
+
+Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const router = new VueRouter();
+
+router.map({
+  '/hello': {
+    component: Hello,
+  },
 });
+
+router.redirect({
+  '*': '/hello',
+});
+
+router.start(App, '#a');
